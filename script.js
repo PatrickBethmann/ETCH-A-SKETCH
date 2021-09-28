f57ba72fedb43ad0192a400a7b8977a6c6dd67a7
Patrick Bethmann <patrickbethmann@outlook.de>
2021-09-28 15:36:16 +0200

    Added a lot. Scalable grid, customizable grid size, drawing function (eventListener)

--- script.js
+++ script.js
@@ -1,11 +1,47 @@
 const container = document.createElement("div"); // Defines Container
 container.className = "container"
+let columns
+let rows
 
-for(let i = 1; i <= 16; i++) {  // Creates 16 divs and appends them to container
-    const squareDiv = document.createElement("div");
-    squareDiv.id = i;
-    squareDiv.className = "pixel";
-    container.appendChild(squareDiv);
+do {
+    columns = null; // Resetting columns and rows so they are not carried over from last attempt
+    rows = null;
+
+    columns = prompt("Width: ");
+    if(columns === "" || columns === null || columns == 0 || isNaN(columns)) continue;
+    rows = prompt("Height: ");
+} while(columns === "" || columns === null || columns == 0 || isNaN(columns) || rows === "" || rows === null || rows == 0 || isNaN(rows));
+
+
+setCanvasSize();0
+
+for(let i = 1; i <= columns*rows; i++) {  // Creates 16 divs and appends them to container
+    const pixel = document.createElement("div");
+    pixel.id = i;
+    pixel.className = "pixel";
+
+    if(columns <= rows) {
+        pixel.style.height = 90/rows+"vh";
+        pixel.style.maxHeight = 90/columns+"vw"; 
+    } else {
+        pixel.style.height = 90/columns+"vw";
+        pixel.style.maxHeight =  90/rows+"vh"; 
+    }
+    container.appendChild(pixel); 
 }
 
-squareDiv = document.body.appendChild(container);
+document.body.appendChild(container);
+
+const pixels = document.querySelectorAll(".pixel");
+pixels.forEach(pixel => {
+    pixel.addEventListener('mouseenter', () => {
+        pixel.classList.add("painted");
+    })
+});
+
+
+
+function setCanvasSize() {
+    container.style.gridTemplateColumns = "repeat("+columns+", 1fr)";
+    container.style.gridTemplateRows = "repeat("+rows+", 1fr)";    
+}
