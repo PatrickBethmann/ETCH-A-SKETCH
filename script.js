e6bf2326c20e9c4af7cd46097c18daab426ba10c
Patrick Bethmann <patrickbethmann@outlook.de>
2021-09-28 10:30:53 +0200

    Added Clear Grid Button

--- script.js
+++ script.js
@@ -1,13 +1,11 @@
-const container = document.createElement("div");
-container.id = "container"
+const container = document.createElement("div"); // Defines Container
+container.className = "container"
 
-for(let i = 1; i <= 16; i++) {
+for(let i = 1; i <= 16; i++) {  // Creates 16 divs and appends them to container
     const squareDiv = document.createElement("div");
     squareDiv.id = i;
     squareDiv.className = "pixel";
     container.appendChild(squareDiv);
-
 }
-
 
 squareDiv = document.body.appendChild(container);
